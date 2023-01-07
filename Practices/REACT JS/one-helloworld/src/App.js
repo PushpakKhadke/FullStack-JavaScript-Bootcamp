3499b5a9b83e72e60d2dd7b8f07aa4c7c48ab43e
bibekghoshh <bibek7478@gmail.com>
2023-01-07 20:51:35 +0530

    complete

---
+++ Practices/REACT JS/one-helloworld/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+
+const App = ()=>{
+    return (
+        <div>
+            <h1 style={{textAlign:"center"}}>Welcome to React Js</h1>
+            <button className="button">Click Me</button>
+        </div>
+    );
+};
+
+export default App;
