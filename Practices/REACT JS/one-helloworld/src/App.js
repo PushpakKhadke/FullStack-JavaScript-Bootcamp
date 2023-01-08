0599a8c9eb9152f021b2fe30778b95c3ec8c9e24
bibekghoshh <bibek7478@gmail.com>
2023-01-08 14:41:33 +0530

    modified

--- Practices/REACT JS/one-helloworld/src/App.js
+++ Practices/REACT JS/one-helloworld/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./style.css";
 
+import Button from "./Button";
 
 const App = ()=>{
     return (
         <div>
             <h1 style={{textAlign:"center"}}>Welcome to React Js</h1>
-            <button className="button">Click Me</button>
+            {/* <button className="button">Click Me</button> */}
+            <Button title="play Store" />
+            <Button title="App Store" />
         </div>
     );
 };
