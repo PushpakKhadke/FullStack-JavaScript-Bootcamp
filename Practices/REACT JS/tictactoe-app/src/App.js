5ad1911fd421f8392f4680ece7ec6bac9c4b3306
bibekghoshh <bibek7478@gmail.com>
2023-01-10 23:25:36 +0530

    new added

---
+++ Practices/REACT JS/tictactoe-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Icon from './Components/Icons';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Icon />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Practices/REACT JS/tictactoe-app/src/Components/Icons.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {FaTimes,FaPen,FaRegCircle} from "react-icons/fa";
+
+const Icon=({name}) =>{
+    return (
+        <div>
+        <h1><FaTimes className='icon'/></h1>
+        <h1><FaPen className='icon'/></h1>
+        <h1><FaRegCircle className='icon'/></h1>
+        </div>
+    )
+};
+
+export default Icon;
