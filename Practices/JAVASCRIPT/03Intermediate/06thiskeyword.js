945a61aeb1ca65f227e3a1be027268e2ab1552ac
bibekghoshh <bibek7478@gmail.com>
2023-02-11 10:30:11 +0530

    modified

--- Practices/JAVASCRIPT/03Intermediate/06thiskeyword.js
+++ Practices/JAVASCRIPT/03Intermediate/06thiskeyword.js
@@ -1,7 +1,24 @@
-console.log(this);
+// console.log(this);  // this refers to global objects
 
 function sayHello(){
     var name="Bibek";
-    console.log(this);
+    // console.log(this);
 }
 sayHello();
+
+//for all regular funtion calls, this points to windows objects
+var u={
+    firstName:"unknown",
+    courseCount:5,
+    getCourseCount:function(){
+        console.log(this.courseCount); //it refers to the u obejct
+        function sayHi(){
+            console.log(this.bibek); //this refers to global obejcts
+            console.log("HI");
+        }
+        sayHi();
+    }
+}
+
+u.getCourseCount();
+
--- Practices/JAVASCRIPT/03Intermediate/09fillterArray.js
+++ Practices/JAVASCRIPT/03Intermediate/09fillterArray.js
@@ -8,7 +8,7 @@ const mynumber=[23,54,12,65,87,23,12,32,54,67];
 var result=mynumber.filter(num=>num<50);
 console.log(result);
 
-slice and splice
+// slice and splice
 console.log(mynumber.slice(3,9));
 
 console.log(mynumber.splice(2,0,"bibek"));
